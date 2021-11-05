8991d0c6a09ec07adac27179ee4c6405e9fc3d77
pasinduf <pasindu.fernan93@gmail.com>
2021-11-05 17:04:32 +0530

    add logger

--- src/student/student-service.ts
+++ src/student/student-service.ts
@@ -5,6 +5,7 @@ import * as moment from 'moment';
 import { StudentResponse } from './dto/student-response';
 import { AddUpdateStudentRequest } from './dto/add-student-request';
 import { getConnection } from 'typeorm';
+import { Logger } from '@nestjs/common';
 
 export class StudentService {
   constructor(
@@ -12,66 +13,84 @@ export class StudentService {
     private studentRepository: Repository<Student>,
   ) {}
 
+  private readonly logger = new Logger(StudentService.name);
+
   async findAll(): Promise<StudentResponse[]> {
     const response: StudentResponse[] = [];
-    const students = await this.studentRepository.find({
-      where: [{ is_active: true }],
-      order: { id: 'DESC' },
-    });
-    students.forEach((student) => {
-      response.push({
-        id: student.id,
-        name: student.name,
-        address: student.address,
-        mobile: student.mobile,
-        dob: student.dob,
-        age: moment().diff(student.dob, 'year'),
-        gender: student.gender === Gender.MALE ? 'male' : 'female',
-        is_active: student.is_active,
+    try {
+      const students = await this.studentRepository.find({
+        where: [{ is_active: true }],
+        order: { id: 'DESC' },
       });
-    });
+      students.forEach((student) => {
+        response.push({
+          id: student.id,
+          name: student.name,
+          address: student.address,
+          mobile: student.mobile,
+          dob: student.dob,
+          age: moment().diff(student.dob, 'year'),
+          gender: student.gender === Gender.MALE ? 'male' : 'female',
+          is_active: student.is_active,
+        });
+      });
+    } catch (ex) {
+      this.logger.log(ex);
+    }
     return response;
   }
 
   save(request: AddUpdateStudentRequest): Promise<Student> {
-    //add logs here
-    const student = {
-      name: request.name,
-      gender: request.gender,
-      address: request.address,
-      mobile: request.mobile,
-      dob: request.dob,
-      is_active: true,
-    };
-    return this.studentRepository.save(student);
+    try {
+      const student = {
+        name: request.name,
+        gender: request.gender,
+        address: request.address,
+        mobile: request.mobile,
+        dob: request.dob,
+        is_active: true,
+      };
+      return this.studentRepository.save(student);
+    } catch (ex) {
+      this.logger.log(ex);
+    }
   }
 
   async saveList(students: AddUpdateStudentRequest[]): Promise<Student[]> {
-    const result = await getConnection()
-      .createQueryBuilder()
-      .insert()
-      .into(Student)
-      .values(students)
-      .execute();
-    return result.identifiers as Student[];
+    try {
+      const result = await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(Student)
+        .values(students)
+        .execute();
+      return result.identifiers as Student[];
+    } catch (ex) {
+      this.logger.log(ex);
+    }
   }
 
   async update(id: number, student: Student): Promise<Student> {
-    const existingStudent = await this.studentRepository.findOne(id);
-    if (!existingStudent) throw new Error("couldn't find student");
-    Object.assign(existingStudent, student);
-    return this.studentRepository.save(existingStudent);
+    try {
+      const existingStudent = await this.studentRepository.findOne(id);
+      if (!existingStudent) throw new Error("couldn't find student");
+      Object.assign(existingStudent, student);
+      return this.studentRepository.save(existingStudent);
+    } catch (ex) {
+      this.logger.log(ex);
+    }
   }
 
   async delete(id: number): Promise<boolean> {
-    const existingStudent = await this.studentRepository.findOne(id);
-    if (!existingStudent) throw new Error("couldn't find student");
-    existingStudent.is_active = false;
-    this.studentRepository.save(existingStudent);
-    return true;
-  }
-
-  findOne(id: number): Promise<Student> {
-    return this.studentRepository.findOne(id);
+    try {
+      const existingStudent = await this.studentRepository.findOne(id);
+      if (!existingStudent) throw new Error("couldn't find student");
+      existingStudent.is_active = false;
+      this.studentRepository.save(existingStudent);
+      return true;
+    } catch (ex) {
+      this.logger.log(ex);
+      return false;
+    }
   }
 }
